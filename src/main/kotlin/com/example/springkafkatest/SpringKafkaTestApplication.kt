cc039931c566a946d346ae12f296a24e0179dd15
BrianDYKim <18k7102dy@naver.com>
2022-07-26 17:52:54 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/springkafkatest/SpringKafkaTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springkafkatest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKafkaTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringKafkaTestApplication>(*args)
+}
